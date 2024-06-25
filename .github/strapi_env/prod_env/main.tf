882945a66a7543015780d41decfe7185ab524ef4
Vyankateshwar Taikar <102132721+VyankateshwarTaikar@users.noreply.github.com>
2024-06-25 22:47:06 +0530

    Update var in main.tf

--- .github/strapi_env/prod_env/main.tf
+++ .github/strapi_env/prod_env/main.tf
@@ -1,16 +1,16 @@
 # Define the AWS key pair resource
 resource "aws_key_pair" "strapi-private-key" {
   key_name   = "strapi-private-key"
-  public_key = var.ssh_public_key  # Update with your public key path
+  public_key = var.ssh_public_key  # Ensure var.ssh_public_key is defined and correct
 }
 
 # Define the AWS instance resource
 resource "aws_instance" "strapi" {
-  ami           = "ami-0705384c0b33c194c"   # Canonical, Ubuntu 22.04 LTS amd64
-  instance_type = "t3.small"
-  key_name      = aws_key_pair.strapi-private-key.key_name
-  subnet_id     = aws_subnet.default.id
-  vpc_security_group_ids = [aws_security_group.strapiSG.id]  # Use vpc_security_group_ids instead of security_group_ids
+  ami                    = "ami-0705384c0b33c194c"  # Verify if this AMI is still available and suitable
+  instance_type          = "t3.small"
+  key_name               = aws_key_pair.strapi-private-key.key_name
+  subnet_id              = aws_subnet.default.id  # Ensure aws_subnet.default is correctly defined
+  vpc_security_group_ids = [aws_security_group.strapiSG.id]  # Ensure aws_security_group.strapiSG is correctly defined
 
   user_data = <<-EOF
     #!/bin/bash
