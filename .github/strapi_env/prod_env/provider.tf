f4d4988b4e3e5a8c66cb363c4ae3577d669eca28
Vyankateshwar Taikar <102132721+VyankateshwarTaikar@users.noreply.github.com>
2024-06-25 22:23:33 +0530

    Updated  provider.tf
    remove 
    var.aws_access_key & secret ky

--- .github/strapi_env/prod_env/provider.tf
+++ .github/strapi_env/prod_env/provider.tf
@@ -3,13 +3,3 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
-
-variable "aws_access_key" {
-  description = "AWS access key"
-  type        = string
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type        = string
-}
