5c4ca37e160a1e9db3e92752c413d797ac92fecd
Vyankateshwar Taikar <102132721+VyankateshwarTaikar@users.noreply.github.com>
2024-06-26 22:25:17 +0530

    Update variable.tf

--- .github/strapi_env/prod_env/variable.tf
+++ .github/strapi_env/prod_env/variable.tf
@@ -1,13 +1,16 @@
 variable "ssh_public_key" {
   type = string
+  default = null
 } 
 
 variable "aws_access_key" {
   description = "AWS access key"
   type        = string
+  default = null
 }
 
 variable "aws_secret_key" {
   description = "AWS secret key"
   type        = string
+  default = null
 }
